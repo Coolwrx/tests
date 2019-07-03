c45c08f6f78b0449e2013b37502ab0a2c282a981
coolwrx <cool_wrx@live.com>
2019-07-04 00:29:48 +0800

    前一个版本问题出在获取用户名上，获取的用户名被 [] 框起来了，show_sslink识别不了

--- go_ss/go_ss.sh
+++ go_ss/go_ss.sh
@@ -6,7 +6,7 @@
 #记得把 ssr_install 里，git clone 命令前的井号删掉
 #天下文章一大抄，这个脚本的部分代码参考了ssrmu.sh
 
-version='0.5.2.1'
+version='0.5.3'
 #定义程序文件夹位置，仅本地测试用
 #ssr_root=~/OneDrive/Codes/github/tests/go_ss   #windows
 #web_root=~/OneDrive/Codes/github/tests/go_ss/home  #windows
@@ -316,6 +316,9 @@ ssr_subscribe(){
                 for ((i=1; i<="${user_info_num}"; i++))
                 do                    
                     name=$(echo "${user_info}"|sed -n "${i}p"|awk '{print $2}')
+                    name=${name#[}  #删除第一个[号，和他左边的所有内容
+                    name=${name%]}  #删除第一个]号，和他右边的所有内容
+
                     show_sslink "${name}"
                     printf "%s" ${sslink} >> sslink.all
                     echo "" >> sslink.all
